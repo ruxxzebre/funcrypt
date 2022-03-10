cd25f171ef6cb46b9ec66707d1685a5073781230
ruxxzebre <paullaboratory@gmail.com>
2022-03-10 14:29:53 +0200

    anotha

--- hashing/src/signatures.ts
+++ hashing/src/signatures.ts
@@ -1,17 +1,28 @@
 import { ec as EC } from "elliptic";
 import * as fs from "fs";
+import path from "path";
 
 const ec = new EC("secp256k1");
 
 const keyPair = ec.genKeyPair();
 
-fs.writeFileSync(__dirname + '/key', JSON.stringify({
-  priv: keyPair.getPrivate(),
-  pub: keyPair.getPublic()
-}));
+const pathToPresentation = path.resolve(__dirname, 'presentation/sha256_chaikovskyiPavlo.key');
+const pathToChangedPresentation = path.resolve(__dirname, 'presentation/sha256_chaikovskyiPablo.key');
 
-const presentationPath = __dirname + '/presentation';
-const fileToSign = fs.readFileSync(__dirname + '/index.ts');
+const presentationBuffer = fs.readFileSync(pathToPresentation);
+const changedPresentationBuffer = fs.readFileSync(pathToChangedPresentation);
 
-const signature = ec.sign(fileToSign, keyPair);
-console.log(signature.toDER('hex'));
+const signature = ec.sign(presentationBuffer, keyPair);
+console.log(`
+Public key: ${keyPair.getPublic("hex")}
+Private key: ${keyPair.getPrivate()}
+
+Signature for 'sha256_chaikovskyiPavlo.key': ${signature.toDER('hex')}
+Signature for 'sha256_chaikovskyiPablo.key': ${signature.toDER('hex')}
+
+Verification:
+
+Verify signature "${signature.toDER('hex')}" of "sha256_chaikovskyiPavlo.key":
+  Public key: 
+    ${ec.keyFromPublic(keyPair.getPublic("hex"), "hex")}
+`)
